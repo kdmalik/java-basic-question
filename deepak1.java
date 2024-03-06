70573dec780678cd72a383c54efddcf5d5372ebc
Deepak Malik <dm2468013@gmail.com>
2024-03-06 23:25:05 +0530

    second java program

--- deepak1.java
+++ deepak1.java
@@ -1,9 +1,18 @@
 /**
  * deepak1
  */
-public class deepak1 {
+// public class deepak1 {
 
-    public static void main(String[] args) {
-        System.out.println("jai shree ram");
-    }
+//     public static void main(String[] args) {
+//         System.out.println("jai shree ram");
+//     }
+// }
+/**
+ * deepak1
+ */
+public class deepak1 {
+public static void main(String[] args) {
+    System.out.println("jai ho baba ki");
+}
+    
 }
