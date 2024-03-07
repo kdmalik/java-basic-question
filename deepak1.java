2012722488ffd2cf5fd95f0e9643bf771a3f22f7
Deepak Malik <dm2468013@gmail.com>
2024-03-07 23:14:27 +0530

    my basic question in java

--- deepak1.java
+++ deepak1.java
@@ -12,7 +12,18 @@
  */
 public class deepak1 {
 public static void main(String[] args) {
-    System.out.println("jai ho baba ki");
+       int a=0;
+       int b=1;
+       int c;
+       System.out.println(a+"\n"+b);
+    for(int i=2;i<10;i++){
+        c=a+b;
+        System.out.println(c);
+
+         a=b;
+         b=c;
+    
+    }
 }
     
 }
