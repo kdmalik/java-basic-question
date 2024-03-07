2e7419b9bd19276148f966263f4c8bc724ad9a35
Deepak Malik <dm2468013@gmail.com>
2024-03-07 23:23:53 +0530

    my basic java question

--- deepak1.java
+++ deepak1.java
@@ -10,20 +10,21 @@
 /**
  * deepak1
  */
-public class deepak1 {
-public static void main(String[] args) {
-       int a=0;
-       int b=1;
-       int c;
-       System.out.println(a+"\n"+b);
-    for(int i=2;i<10;i++){
-        c=a+b;
-        System.out.println(c);
+//fibonacci series
+// public class deepak1 {
+// public static void main(String[] args) {
+//        int a=0;
+//        int b=1;
+//        int c;
+//        System.out.println(a+"\n"+b);
+//     for(int i=2;i<10;i++){
+//         c=a+b;
+//         System.out.println(c);
 
-         a=b;
-         b=c;
+//          a=b;
+//          b=c;
     
-    }
-}
+//     }
+// }
     
-}
+// }
